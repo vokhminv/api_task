818e88d11803298cbb2e784e86e0e3b88ee6386d
vokhminv <vlad.vokhmin@icloud.com>
2023-11-02 17:21:05 +0200

    updated CatalogScreen composable layout

--- app/src/main/java/com/mintsdev/api_task/ui/theme/screens/CatalogScreen.kt
+++ app/src/main/java/com/mintsdev/api_task/ui/theme/screens/CatalogScreen.kt
@@ -1,40 +1,139 @@
 package com.mintsdev.api_task.ui.theme.screens
 
-import android.content.Context
-import android.util.Log
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Card
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.mintsdev.api_task.datastore.StoreManager
+import com.mintsdev.api_task.R
+import com.mintsdev.api_task.api.BrandResponse
 import com.mintsdev.api_task.ui.theme.viewmodel.CatalogViewModel
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CatalogScreen(viewModel: CatalogViewModel, navController: NavController){
+fun CatalogScreen(viewModel: CatalogViewModel, navController: NavController) {
     val storedToken by viewModel.tokenFlow.collectAsState(initial = null)
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                navigationIcon = {
+                    IconButton(onClick = {
+                        navController.navigate("loginScreen")
+                    }) {
+                        Icon(
+                            Icons.Default.ArrowBack,
+                            contentDescription = "ArrowBack")
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { }) {
+                        Icon(
+                            Icons.Default.Search,
+                            contentDescription = "Search")
+                    }
+                    IconButton(onClick = { }) {
+                        Icon(
+                            Icons.Default.Settings,
+                            contentDescription = "Settings")
+                    }
 
+                },
+                title = {
+                    Text(
+                        fontSize = 12.sp,
+                        text = "Token: $storedToken")
+                }
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = {}) {
+                Icon(Icons.Default.Add, contentDescription = "Add")
+            }
+        }
+    ) { innerPadding ->
+        Row(
+            modifier = Modifier
+                .padding(innerPadding),
+            horizontalArrangement = Arrangement.Center,
+        ) {
+            LazyVerticalGrid(
+                modifier = Modifier.fillMaxSize(),
+                contentPadding = PaddingValues(16.dp),
+                columns = GridCells.Adaptive(minSize = 128.dp)
+            ){
+                items(count = 50) { cardData ->
+                    Card(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp),
+                        shape = RoundedCornerShape(16.dp)
+                    ) {
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .wrapContentSize(Alignment.Center)
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                                contentDescription = null,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .height(85.dp)
+                                    .width(85.dp)
+                                    .clip(shape = RoundedCornerShape(16.dp)),
+                                contentScale = ContentScale.Fit
+                            )
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text("Token: $storedToken")
+                            Spacer(modifier = Modifier.height(8.dp))
+
+                            Text(
+                                text = "BrandResponse.brandName",
+                                fontSize = 18.sp,
+                                modifier = Modifier
+                                    .align(Alignment.CenterHorizontally))
+                            Spacer(modifier = Modifier.height(8.dp))
+                        }
+                    }
+                }
+            }
+        }
     }
+
+
+
 }
